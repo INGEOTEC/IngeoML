766154c7ede010f95248e75609eb5ac65beee7e4
Mario Graff <mgraffg@ieee.org>
2024-01-16 12:40:15 -0600

    improving tests

--- IngeoML/tests/test_optimizer.py
+++ IngeoML/tests/test_optimizer.py
@@ -80,13 +80,15 @@ def test_classifier():
     evol = np.array([x[1] for x in evol])
     assert np.any(np.diff(evol) != 0)
     X, y = load_breast_cancer(return_X_y=True)
-    m = LinearSVC(dual='auto').fit(X, y)
+    index = np.arange(X.shape[0])
+    np.random.shuffle(index)    
+    m = LinearSVC(dual='auto').fit(X[index[:400]], y[index[:400]])
     parameters = dict(W=jnp.array(m.coef_.T),
                       W0=jnp.array(m.intercept_))
-    p2 = classifier(parameters, modelo, X, y)
-    assert np.fabs(p2['W'] - parameters['W']).sum() > 0
-    diff = p2['W0'] - parameters['W0']
-    assert np.fabs(diff).sum() > 0
+    p2, evol = classifier(parameters, modelo, X, y,
+                          return_evolution=True)
+    evol = np.array([x[1] for x in evol])
+    assert np.any(np.diff(evol) != 0)
 
 
 def test_regression():
